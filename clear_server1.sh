0541c0396d2e7fb75ea5756cd185dadaef1e9a6b
a42dab9659090cac <a42dab9659090cac@outlook.com>
2018-08-07 10:16:58 +0800

    add bios addr

--- clear_server1.sh
+++ clear_server1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd ./pcbpa && ./clean.sh && cd ..
+cd ./pcbpb && ./clean.sh && cd ..
+cd ./pcbpc && ./clean.sh && cd ..
 cd ./pcbpd && ./clean.sh && cd ..
 cd ./pcbpe && ./clean.sh && cd ..
 cd ./pcbpf && ./clean.sh && cd ..
 cd ./pcbpg && ./clean.sh && cd ..
 cd ./pcbph && ./clean.sh && cd ..
 cd ./pcbpi && ./clean.sh && cd ..
-cd ./pcbpj && ./clean.sh && cd ..
-cd ./pcbpk && ./clean.sh && cd ..
