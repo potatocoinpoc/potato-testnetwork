2207d35621b657d0a6a54f00c878621af9d63e8b
a42dab9659090cac <a42dab9659090cac@outlook.com>
2018-08-07 10:56:00 +0800

    update

--- run_server2.sh
+++ run_server2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#cd ./pcbpj/ && screen -dmS pcbpj ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpj/ && screen -dmS pcbpj ./run.sh && cd .. && ping 127.0.0.1 -c 1
 cd ./pcbpk/ && screen -dmS pcbpk ./run.sh && cd .. && ping 127.0.0.1 -c 1
 cd ./pcbpl/ && screen -dmS pcbpl ./run.sh && cd .. && ping 127.0.0.1 -c 1
 cd ./pcbpm/ && screen -dmS pcbpm ./run.sh && cd .. && ping 127.0.0.1 -c 1
