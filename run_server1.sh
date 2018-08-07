c6cfbaae2cdca092ff335ad396f991e03df3412c
rise <rise.worlds@outlook.com>
2018-08-07 10:29:59 +0800

    update

--- run_server1.sh
+++ run_server1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd  ./pcbpa && screen -dmS pcbpa ./run.sh && cd ..
-cd  ./pcbpd && screen -dmS pcbpd ./run.sh && cd ..
-cd  ./pcbpe && screen -dmS pcbpe ./run.sh && cd ..
-cd  ./pcbpf && screen -dmS pcbpf ./run.sh && cd ..
-cd  ./pcbpg && screen -dmS pcbpg ./run.sh && cd ..
-cd  ./pcbph && screen -dmS pcbph ./run.sh && cd ..
-cd  ./pcbpi && screen -dmS pcbpi ./run.sh && cd ..
-cd  ./pcbpj && screen -dmS pcbpj ./run.sh && cd ..
-cd  ./pcbpk && screen -dmS pcbpk ./run.sh && cd ..
+cd  ./pcbpa && screen -dmS pcbpa ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd  ./pcbpb && screen -dmS pcbpb ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd  ./pcbpc && screen -dmS pcbpc ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd  ./pcbpd && screen -dmS pcbpd ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd  ./pcbpe && screen -dmS pcbpe ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd  ./pcbpf && screen -dmS pcbpf ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd  ./pcbpg && screen -dmS pcbpg ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd  ./pcbph && screen -dmS pcbph ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd  ./pcbpi && screen -dmS pcbpi ./run.sh && cd .. && ping 127.0.0.1 -c 1
--- run_server3.sh
+++ run_server3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd ./pcbpc && screen -dmS pcbpc ./run.sh && cd ..
-cd ./pcbpt && screen -dmS pcbpt ./run.sh && cd ..
-cd ./pcbpu && screen -dmS pcbpu ./run.sh && cd ..
-cd ./pcbpv && screen -dmS pcbpv ./run.sh && cd ..
-cd ./pcbpw && screen -dmS pcbpw ./run.sh && cd ..
-cd ./pcbpx && screen -dmS pcbpx ./run.sh && cd ..
-cd ./pcbpy && screen -dmS pcbpy ./run.sh && cd ..
-cd ./pcbpz && screen -dmS pcbpz ./run.sh && cd ..
+cd ./pcbps && screen -dmS pcbps ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpt && screen -dmS pcbpt ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpu && screen -dmS pcbpu ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpv && screen -dmS pcbpv ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpw && screen -dmS pcbpw ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpx && screen -dmS pcbpx ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpy && screen -dmS pcbpy ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpz && screen -dmS pcbpz ./run.sh && cd .. && ping 127.0.0.1 -c 1
