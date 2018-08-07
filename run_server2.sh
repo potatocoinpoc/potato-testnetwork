867561c7e88f3efdacccb41ab41876d55b1d93a7
a42dab9659090cac <a42dab9659090cac@outlook.com>
2018-08-07 10:29:59 +0800

    update

--- run_server2.sh
+++ run_server2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd ./pcbpb/ && screen -dmS pcbpb ./run.sh && cd ..
-cd ./pcbpl/ && screen -dmS pcbpl ./run.sh && cd ..
-cd ./pcbpm/ && screen -dmS pcbpm ./run.sh && cd ..
-cd ./pcbpn/ && screen -dmS pcbpn ./run.sh && cd ..
-cd ./pcbpo/ && screen -dmS pcbpo ./run.sh && cd ..
-cd ./pcbpp/ && screen -dmS pcbpp ./run.sh && cd ..
-cd ./pcbpq/ && screen -dmS pcbpq ./run.sh && cd ..
-cd ./pcbpr/ && screen -dmS pcbpr ./run.sh && cd ..
-cd ./pcbps/ && screen -dmS pcbps ./run.sh && cd ..
+cd ./pcbpj/ && screen -dmS pcbpj ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpk/ && screen -dmS pcbpk ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpl/ && screen -dmS pcbpl ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpm/ && screen -dmS pcbpm ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpn/ && screen -dmS pcbpn ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpo/ && screen -dmS pcbpo ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpp/ && screen -dmS pcbpp ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpq/ && screen -dmS pcbpq ./run.sh && cd .. && ping 127.0.0.1 -c 1
+cd ./pcbpr/ && screen -dmS pcbpr ./run.sh && cd .. && ping 127.0.0.1 -c 1
