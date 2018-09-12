19a38fb6c20d2f373085e64a231220051488d7e7
a42dab9659090cac <a42dab9659090cac@outlook.com>
2018-09-12 23:42:48 +0800

    123

--- eosnetwork/run_116.31.75.85.sh
+++ eosnetwork/run_116.31.75.85.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-screen -dmS pcbpk nodepc --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./pcbpk \
+screen -dmS pcbpk nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./pcbpk \
     --p2p-listen-endpoint 0.0.0.0:9000 --agent-name "Agent pcbpk" --producer-name pcbpk \
     --plugin potato::http_plugin --plugin potato::chain_api_plugin --access-control-allow-origin "*" --http-server-address "0.0.0.0:8888" \
     --signature-provider "EOS7WnhaKwHpbSidYuh2DF1qAExTRUtPEdZCaZqt75cKcixuQUtdA=KEY:5K6LU8aVpBq9vJsnpCvaHCcyYwzPPKXfDdyefYyAMMs3Qy42fUr"
 
 #screen -dmS pcbpv 
-nodepc --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./pcbpv \
+nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./pcbpv \
     --p2p-listen-endpoint 0.0.0.0:9001 --agent-name "Agent pcbpv" --producer-name pcbpv \
     --signature-provider "EOS5y3Tm1czTCia3o3JidVKmC78J9gRQU8qHjmRjFxTyhh2vxvF5d=KEY:5JJjaKnAb9KM2vkkJDgrYXoeUEdGgWtB5WK1a38wrmKnS3KtkS6"
--- eosnetwork/run_119.63.39.63.sh
+++ eosnetwork/run_119.63.39.63.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 screen -dmS pcbpl 
-nodepc --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./pcbpl \
+nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./pcbpl \
     --p2p-listen-endpoint 0.0.0.0:9000 --agent-name "Agent pcbpl" --producer-name pcbpl \
     --plugin potato::http_plugin --plugin potato::chain_api_plugin --access-control-allow-origin "*" --http-server-address "0.0.0.0:8888" \
     --signature-provider "EOS7Bn1YDeZ18w2N9DU4KAJxZDt6hk3L7eUwFRAc1hb5bp6xJwxNV=KEY:5KdRpt1juJfbPEryZsQYxyNxSTkXTdqEiL4Yx9cAjdgApt4ANce"
