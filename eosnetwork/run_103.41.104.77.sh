f335c499b825a147bcb0bd0e839eed34c3f57d28
rise <rise.worlds@outlook.com>
2018-09-13 10:09:42 +0800

    1234

--- eosnetwork/run_103.41.104.77.sh
+++ eosnetwork/run_103.41.104.77.sh
@@ -5,7 +5,13 @@ screen -dmS pcbpd nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --c
     --plugin eosio::http_plugin --plugin eosio::chain_api_plugin --access-control-allow-origin "*" --http-server-address "0.0.0.0:8888" \
     --signature-provider "EOS7ZH8veauyEUu4dJU5bTRSPHFctiFkV3Fyaj1tWsr83Jf6dmXh8=KEY:5JhsjUjKUJXYSvPWAZe23ceNSMdjk9U32jjmgFTZfXxe4yV2y4A"
 
-#screen -dmS pcbpl 
-nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./pcbpl \
-    --p2p-listen-endpoint 0.0.0.0:9001 --agent-name "Agent pcbpl" --producer-name pcbpl \
-    --signature-provider "EOS7Bn1YDeZ18w2N9DU4KAJxZDt6hk3L7eUwFRAc1hb5bp6xJwxNV=KEY:5KdRpt1juJfbPEryZsQYxyNxSTkXTdqEiL4Yx9cAjdgApt4ANce"
+screen -dmS pcbpr nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./pcbpr \
+    --p2p-listen-endpoint 0.0.0.0:9001 --agent-name "Agent pcbpr" --producer-name pcbpr \
+    --plugin eosio::http_plugin --plugin eosio::chain_api_plugin --access-control-allow-origin "*" --http-server-address "0.0.0.0:8899" \
+    --signature-provider "EOS8FdMPpPxpG5QAqGLncY5kBrEQ9NXPKCKnLH6oWDMPR8q8BrEmT=KEY:5K4MmsY7Th8DqjEY2vbM7npaxSQ56XzvNULkJeqKmbYoVRmPPpB"
+
+#screen -dmS pcbpx 
+nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./pcbpx \
+    --p2p-listen-endpoint 0.0.0.0:9002 --agent-name "Agent pcbpx" --producer-name pcbpx \
+    --plugin eosio::http_plugin --plugin eosio::chain_api_plugin --access-control-allow-origin "*" --http-server-address "0.0.0.0:8999" \
+    --signature-provider "EOS76wwc1zjzRMPQnEL6rTDWLfhN2ByZd6GhDJoWBKWM6M7cUmyfS=KEY:5K3TXkZAwyJkg7TjSfopd7sTr3RXFccghXHN1nJHTzap1ZKgLdK"
--- eosnetwork/run_121.14.88.140.sh
+++ eosnetwork/run_121.14.88.140.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-screen -dmS pcbpn nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./pcbpn \
+#screen -dmS pcbpn 
+nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./pcbpn \
     --p2p-listen-endpoint 0.0.0.0:9000 --agent-name "Agent pcbpn" --producer-name pcbpn \
     --plugin eosio::http_plugin --plugin eosio::chain_api_plugin --access-control-allow-origin "*" --http-server-address "0.0.0.0:8888" \
     --signature-provider "EOS8UkmsnCo4GxDihbKwgoZY6f2QLSMEqBZ2frGLckxrCHrz15r7X=KEY:5HqyipkJSm5fwYhbhGC3vmmoBwabtgJSPecnvmN2mMrCTQfWBSS"
-
-#screen -dmS pcbpx 
-nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./ --data-dir ./pcbpx \
-    --p2p-listen-endpoint 0.0.0.0:9001 --agent-name "Agent pcbpx" --producer-name pcbpx \
-    --signature-provider "EOS76wwc1zjzRMPQnEL6rTDWLfhN2ByZd6GhDJoWBKWM6M7cUmyfS=KEY:5K3TXkZAwyJkg7TjSfopd7sTr3RXFccghXHN1nJHTzap1ZKgLdK"
