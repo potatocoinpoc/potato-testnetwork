e472a53b883091af8b6cd337c7ef66a6f1e0ab7d
a42dab9659090cac <a42dab9659090cac@outlook.com>
2018-09-13 13:23:28 +0800

    123

--- eosnetwork/run_116.31.75.85.sh
+++ eosnetwork/run_116.31.75.85.sh
@@ -3,5 +3,5 @@
 #screen -dmS pcbpk 
 nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./pcbpk --data-dir ./pcbpk \
     --p2p-listen-endpoint 0.0.0.0:9000 --agent-name "Agent pcbpk" --producer-name pcbpk \
-    --plugin eosio::http_plugin --plugin eosio::chain_api_plugin --access-control-allow-origin "*" --http-server-address "0.0.0.0:8888" \
+    --http-server-address "0.0.0.0:8888" \
     --signature-provider "EOS7WnhaKwHpbSidYuh2DF1qAExTRUtPEdZCaZqt75cKcixuQUtdA=KEY:5K6LU8aVpBq9vJsnpCvaHCcyYwzPPKXfDdyefYyAMMs3Qy42fUr"
--- eosnetwork/run_119.63.39.63.sh
+++ eosnetwork/run_119.63.39.63.sh
@@ -3,6 +3,6 @@
 screen -dmS pcbpl 
 nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./pcbpl --data-dir ./pcbpl \
     --p2p-listen-endpoint 0.0.0.0:9000 --agent-name "Agent pcbpl" --producer-name pcbpl \
-    --plugin eosio::http_plugin --plugin eosio::chain_api_plugin --access-control-allow-origin "*" --http-server-address "0.0.0.0:8888" \
+    --http-server-address "0.0.0.0:8888" \
     --signature-provider "EOS7Bn1YDeZ18w2N9DU4KAJxZDt6hk3L7eUwFRAc1hb5bp6xJwxNV=KEY:5KdRpt1juJfbPEryZsQYxyNxSTkXTdqEiL4Yx9cAjdgApt4ANce"
 
