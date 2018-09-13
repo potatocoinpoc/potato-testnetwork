54cedf32ce33bceb7ca26b91d747bda3cbf131ea
rise <rise.worlds@outlook.com>
2018-09-13 13:23:28 +0800

    123

--- eosnetwork/run_121.14.88.140.sh
+++ eosnetwork/run_121.14.88.140.sh
@@ -3,5 +3,5 @@
 #screen -dmS pcbpn 
 nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./pcbpn --data-dir ./pcbpn \
     --p2p-listen-endpoint 0.0.0.0:9000 --agent-name "Agent pcbpn" --producer-name pcbpn \
-    --plugin eosio::http_plugin --plugin eosio::chain_api_plugin --access-control-allow-origin "*" --http-server-address "0.0.0.0:8888" \
+    --http-server-address "0.0.0.0:8888" \
     --signature-provider "EOS8UkmsnCo4GxDihbKwgoZY6f2QLSMEqBZ2frGLckxrCHrz15r7X=KEY:5HqyipkJSm5fwYhbhGC3vmmoBwabtgJSPecnvmN2mMrCTQfWBSS"
--- eosnetwork/run_121.14.88.149.sh
+++ eosnetwork/run_121.14.88.149.sh
@@ -3,6 +3,6 @@
 #screen -dmS pcbpp 
 nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./pcbpp --data-dir ./pcbpp \
     --p2p-listen-endpoint 0.0.0.0:9000 --agent-name "Agent pcbpp" --producer-name pcbpp \
-    --plugin eosio::http_plugin --plugin eosio::chain_api_plugin --access-control-allow-origin "*" --http-server-address "0.0.0.0:8888" \
+    --http-server-address "0.0.0.0:8888" \
     --signature-provider "EOS57VTWSiPyx45cSWGdGNtAZnmpqMrAvASQmL9hmXnoLNrgadwf7=KEY:5JV9UNEpPKa4sqxSxvGWYPY9ZBTzAttyq7ShPvLUJSetwAeSXFW"
 
