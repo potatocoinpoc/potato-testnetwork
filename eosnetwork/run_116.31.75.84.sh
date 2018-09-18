0ba7c481f9d21809c23619a106fd67417b885e1d
rise <rise.worlds@outlook.com>
2018-09-18 11:16:26 +0800

    reset port

--- eosnetwork/run_116.31.75.84.sh
+++ eosnetwork/run_116.31.75.84.sh
@@ -6,6 +6,6 @@ screen -dmS pcbpj nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --c
     --signature-provider "EOS7yBtksm8Kkg85r4in4uCbfN77uRwe82apM8jjbhFVDgEgz3w8S=KEY:5JUNYmkJ5wVmtVY8x9A1KKzYe9UWLZ4Fq1hzGZxfwfzJB8jkw6u"
 
 screen -dmS pcbpz nodeos --genesis-json ./genesis.json --blocks-dir ./blocks --config-dir ./pcbpz --data-dir ./pcbpz \
-    --p2p-listen-endpoint 0.0.0.0:9002 --agent-name "Agent pcbpz" --producer-name pcbpz \
+    --p2p-listen-endpoint 0.0.0.0:9001 --agent-name "Agent pcbpz" --producer-name pcbpz \
     --http-server-address "0.0.0.0:8999" \
     --signature-provider "EOS8ZjbDEi872aLpuuAjnd76NYW6KzPaf6RBSuwXcHmKm7A1sxayV=KEY:5JrC4vnVXroqvXtmSw3EiXcSv9agfDsKsUghzv2Rhiyrg9J36Z4"
